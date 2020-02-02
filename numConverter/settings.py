69e0a5392245cccd5d00114c5fe0a8f98e83462c
mweems <weemsmatthewj@gmail.com>
2020-02-02 09:14:00 -0800

    update debug

--- numConverter/settings.py
+++ numConverter/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sgt6ka!@=s(#a3l+vwhj+b&li2%qwvqak+va=4pn@=f&1siwda'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
